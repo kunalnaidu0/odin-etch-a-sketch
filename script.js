8e59b5d222fea63581106c77fae7372a36204af8
kunalnaidu <kunalnaidu@live.ca>
2024-10-29 10:28:00 -0600

    Adds Ability to Change Grid Size
    Creates a button called change grid size that when clicks prompts the
    user to add a new valid grid size and deletes the previous grid to
    create a new grid.

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const grid = document.querySelector('.grid');
 
-const gridSize = 10;
+// constant values
 const fill = 'black';
 
-for (let i = 0; i < gridSize; i++){
-    const divRow = document.createElement('div');
-    divRow.setAttribute('style', `height:${grid.clientHeight/gridSize}px`);
-    divRow.setAttribute('class', 'row');
+
+function createGrid(gridSize = 10){
+    // delete previous grid
+    grid.replaceChildren();
+    // create the grid
     for (let i = 0; i < gridSize; i++){
-        const div = document.createElement('div');
-        div.setAttribute('class', 'tile');
-        div.onmouseover = () => div.setAttribute("style", `background-color: ${fill}`);
-        divRow.appendChild(div);
-    }
-    
-    grid.appendChild(divRow);
-    
+        const divRow = document.createElement('div');
+        divRow.setAttribute('style', `height:${grid.clientHeight/gridSize}px`);
+        divRow.setAttribute('class', 'row');
+        for (let i = 0; i < gridSize; i++){
+            const div = document.createElement('div');
+            div.setAttribute('class', 'tile');
+            // add hover effect
+            div.onmouseover = () => div.setAttribute("style", `background-color: ${fill}`);
+            divRow.appendChild(div);
+        }
+        
+        grid.appendChild(divRow);
+        
+
+    };
+
 }
+
+createGrid();
+
+
+const changeSize = document.querySelector("#grid-size");
+changeSize.onclick = function(){
+
+    let newSize = prompt("Please enter a new grid size");
+
+    while (newSize <= 0 || newSize > 100 || newSize % 1 != 0){
+        alert("Please enter a integer between 1 and 100");
+        newSize = prompt("Please enter a new grid size");
+    }
+    createGrid(newSize);
+
+};
+
+
