c018ab4be16da2b0f8e6d4c66c20be0e0c73d081
kunalnaidu <kunalnaidu@live.ca>
2024-10-29 11:13:55 -0600

    Adds Earaser, Clear, and Color Picker
    Adds Eraser button which turns hovered tile white when the button is
    selected. The Clear button changes the whole grid back to white. The
    color picker when changed causes the grid to be filled in the selected
    color.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const grid = document.querySelector('.grid');
 
 // constant values
-const fill = 'black';
+let fill = '#000000';
 
-
+/**
+ * Creates grid and deletes any previous grid that was present. Uses the div with the class 'grid''.
+ * @param {Number} gridSize 
+ */
 function createGrid(gridSize = 10){
+
     // delete previous grid
     grid.replaceChildren();
+
     // create the grid
     for (let i = 0; i < gridSize; i++){
         const divRow = document.createElement('div');
@@ -18,29 +23,57 @@ function createGrid(gridSize = 10){
             // add hover effect
             div.onmouseover = () => div.setAttribute("style", `background-color: ${fill}`);
             divRow.appendChild(div);
-        }
-        
+        }      
         grid.appendChild(divRow);
-        
-
     };
 
 }
 
 createGrid();
 
-
+// create new grid with new size
 const changeSize = document.querySelector("#grid-size");
 changeSize.onclick = function(){
 
     let newSize = prompt("Please enter a new grid size");
-
+    // check to see if grid size given follows rules
     while (newSize <= 0 || newSize > 100 || newSize % 1 != 0){
         alert("Please enter a integer between 1 and 100");
         newSize = prompt("Please enter a new grid size");
-    }
+    };
     createGrid(newSize);
 
 };
 
 
+// make clear button turn all tiles in grid to white
+const clearGrid = document.querySelector("#clear");
+clearGrid.onclick = function(){
+    const tiles = document.querySelectorAll(".tile");
+    for (const tile of tiles) tile.setAttribute("style", "background-color: white");
+}
+
+// make eraser button turn fill to white
+const eraser = document.querySelector("#eraser");
+eraser.onclick = () => fill = 'white';
+
+
+// make color picker change fill to selected color
+let colorPicker;
+const defaultColor = fill;
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+    colorPicker = document.querySelector("#color-picker");
+    colorPicker.value = defaultColor;
+    colorPicker.addEventListener("input", update, false);
+    colorPicker.select();
+  }
+
+  function update(event) {
+      fill = event.target.value;
+  };
+  
+
+  
